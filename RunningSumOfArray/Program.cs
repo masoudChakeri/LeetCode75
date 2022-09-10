aa8634c01df3ff499bf0a0b973d71587760f2a9e
masoudChakeri <masoud.chakeri@gmail.com>
2022-09-10 21:19:51 +0430

    Running Sum of 1d Array submitted

---
+++ RunningSumOfArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] RunningSum(int[] nums)
+{
+    int[] sumArray = new int[nums.Length];
+    var sum = 0;
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        sum += nums[i];
+        sumArray[i] = sum;
+    }
+
+    return sumArray;
+}
+
+Array.ForEach(RunningSum(new[] { 1, 2, 3, 4 }), Console.WriteLine);
